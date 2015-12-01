480a1220258016935683d1c1f0d03ed538246f90
David Santiago Fdez-Bermejo Morales <david@davidsantiago.net>
2015-12-01 12:25:32 +0100

    Añadido --force a fichero start.js

--- start.js
+++ start.js
@@ -3,7 +3,7 @@ var pm2 = require('pm2');
 pm2.connect(function() {
   pm2.start({
     script    : '/usr/bin/grunt',         // Script to be run
-    args: 'server',
+    args: 'server --force',
   }, function(err, apps) {
     pm2.disconnect();
   });
